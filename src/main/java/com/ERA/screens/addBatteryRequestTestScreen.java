ca87d1252ef04d76e32eddf93c62984d2d4592fa
amira <amira@AS-40.arabiansystems.com>
2022-03-20 13:49:31 +0200

    commit 20/3

---
+++ src/main/java/com/ERA/screens/addBatteryRequestTestScreen.java
@@ -0,0 +1,82 @@
+package com.ERA.screens;
+
+import org.openqa.selenium.interactions.Actions;
+
+import com.ERA.base.Base;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+
+public class addBatteryRequestTestScreen extends Base {
+
+	@AndroidFindBy(xpath = "//android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.Button")
+	public MobileElement From;
+	@AndroidFindBy(id = "com.eci.era:id/clAddress")
+	public MobileElement To;
+
+	public void dragsheet() {
+		Actions act = new Actions(driver);
+		act.dragAndDrop(From, To).build().perform();
+
+	}
+	
+	@AndroidFindBy(uiAutomator = "new UiSelector().text(\"إعادة تشغيل البطارية\")")
+	public MobileElement Battery;
+
+	public void selectBattery() {
+		Battery.click();
+	
+
+	}
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().text(\"حدد طريقة الدفع\")")
+
+	public MobileElement ConfirmSummery;
+
+	public void cnfirmSummery() {
+
+		ConfirmSummery.click();
+
+	}
+
+	@AndroidFindBy(id = "com.eci.era:id/continueBtn")
+	public MobileElement ConfirmBaymnet;
+
+	public void ConfmBaymnet() {
+
+		ConfirmBaymnet.click();
+
+	}
+
+	@AndroidFindBy(id = "com.eci.era:id/tvCancelRequest")
+	public MobileElement CancelLk;
+
+	public void Cancellink() {
+
+		CancelLk.click();
+
+	}
+
+	@AndroidFindBy(id = "com.eci.era:id/btnCancel")
+	public MobileElement cnclBn;
+
+	public void CancelBn() {
+
+		cnclBn.click();
+
+	}
+
+	
+		@AndroidFindBy(id = "com.eci.era:id/clAddress")
+		public MobileElement MapElement1;
+		@AndroidFindBy(id = "com.eci.era:id/marker")
+		public MobileElement MapElement2;
+
+		public void mapmove() {
+			Actions act = new Actions(driver);
+			act.dragAndDrop(MapElement2, MapElement1).build().perform();
+
+		}	
+	
+
+}
---
+++ src/test/java/com/ERA/testcases/addNormalBatteryRequestTest.java
@@ -0,0 +1,84 @@
+package com.ERA.testcases;
+
+import org.testng.annotations.Test;
+
+import com.ERA.base.Base;
+import com.ERA.screens.addBatteryRequestTestScreen;
+
+public class addNormalBatteryRequestTest  extends Base {
+
+	
+	@Test(priority = 29)
+	public void MoveMap()
+
+	{
+		addBatteryRequestTestScreen map = new addBatteryRequestTestScreen();
+		map.mapmove();
+		
+
+	}
+   
+	@Test(priority = 30)
+	public void DragServiceSheet()
+
+	{
+		addBatteryRequestTestScreen Battery = new addBatteryRequestTestScreen();
+		Battery.dragsheet();
+		
+
+	}
+
+	@Test(priority = 31)
+	public void SelectBattery()
+
+	{
+		addBatteryRequestTestScreen Battery = new addBatteryRequestTestScreen();
+		Battery.selectBattery();
+		
+
+	}
+   
+	
+	
+	
+	@Test(priority = 32)
+	public void ConfirmSummery()
+
+	{
+		addBatteryRequestTestScreen confrmSummery = new addBatteryRequestTestScreen();
+		confrmSummery.cnfirmSummery();
+		
+
+	}
+	
+
+	@Test(priority = 33)
+	public void ConfirmBaymnet()
+
+	{
+		addBatteryRequestTestScreen confirmSummery = new addBatteryRequestTestScreen();
+		confirmSummery.ConfmBaymnet();
+		
+
+	}
+	
+	@Test(priority = 34)
+	public void CancelLink()
+
+	{
+		addBatteryRequestTestScreen cancelLnk = new addBatteryRequestTestScreen();
+		cancelLnk.Cancellink();
+		
+
+	}
+	
+	@Test(priority = 35)
+	public void CancelBn()
+
+	{
+		addBatteryRequestTestScreen cancelBn = new addBatteryRequestTestScreen();
+		cancelBn.CancelBn();
+		
+
+	}
+}
