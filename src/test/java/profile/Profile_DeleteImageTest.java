4bc2fab8a62f158f6014b9ef0182d92cb0327cc0
amira <amira@AS-40.arabiansystems.com>
2022-04-01 23:08:26 +0200

    commit 1-4-2022

---
+++ src/test/java/profile/Profile_DeleteImageTest.java
@@ -0,0 +1,63 @@
+package profile;
+
+import com.ERA.base.Base;
+import com.ERA.screens.MenuScreen;
+import com.ERA.screens.profile.profiletabFromMenu;
+
+import org.testng.annotations.Test;
+
+public class Profile_DeleteImageTest extends Base {
+
+
+	@Test(priority = 1)
+	public void clickprofiletabFromMenu()
+
+	{
+		profiletabFromMenu profile = new profiletabFromMenu();
+		profile.clickprofiletabFromMenu();
+
+	}
+	@Test(priority = 2)
+	public void ImageOtions()
+
+	{
+		profiletabFromMenu ImageOtions = new profiletabFromMenu();
+		ImageOtions.ClickImageOptions();
+	
+	}
+	@Test(priority = 3)
+	public void AllowPermissionImageTake()
+
+	{
+		profiletabFromMenu allowPermission = new profiletabFromMenu();
+		allowPermission.ClickAllowPermission();
+	
+	}
+	
+	@Test(priority = 4)
+	public void AllowPermissionaccessFiles()
+
+	{
+		profiletabFromMenu allowPermissionaccessFiles = new profiletabFromMenu();
+		allowPermissionaccessFiles.ClickAllowPermission();
+	
+	}
+	
+	@Test(priority = 5)
+	public void DeleteImage()
+
+	{
+		profiletabFromMenu DeleteImage = new profiletabFromMenu();
+		DeleteImage.ClickDeleteImage();
+
+	}
+	@Test(priority = 6)
+	public void ClickMenu()
+
+	{
+		MenuScreen Menu = new MenuScreen();
+		Menu.ClickMenuIcon();
+
+	}
+
+}
